6fccb7ff94e2f40c8f7d9342f42349b69209871f
alexander_mushtukov <31170808+alexandermushtukov@users.noreply.github.com>
2020-04-13 10:56:19 +0200

    AstroFun creation

---
+++ af_ref_frame.f90
@@ -0,0 +1,36 @@
+!=================================================================================================================!
+! Angle transformation from B-field reference frame to observer's reference frame
+! The function returns latitude in observer's RF:
+! i.e. the angle between direction to the observer and direction to the point on the NS surface
+!=================================================================================================================!
+real*8 function B2obs(ksi,theta_B,fi_B)
+implicit none
+real*8,intent(in)::ksi,theta_B,fi_B
+  B2obs=acos(cos(theta_B)*cos(ksi)-sin(fi_B)*sin(theta_B)*sin(ksi))
+return
+end function B2obs
+!=================================================================================================================!
+
+
+!=================================================================================================================!
+! Azimutal angle in observer's RF.
+!=================================================================================================================!
+real*8 function B2obs_fi(ksi,theta_B,fi_B)
+implicit none
+real*8,intent(in)::ksi,theta_B,fi_B
+real*8::B2obs                !==function==!
+real*8::theta_0,sin_fi0,cos_fi0
+  theta_0=B2obs(ksi,theta_B,fi_B)
+  if(sin(theta_0).eq.0.d0)then
+    B2obs_fi=0.d0
+  else
+    sin_fi0=(sin(fi_B)*sin(theta_B)*cos(ksi)+cos(theta_B)*sin(ksi))/sin(theta_0)
+    cos_fi0=cos(fi_B)*sin(theta_B)/sin(theta_0)
+    B2obs_fi=acos(max(-1.d0,min(cos_fi0,1.d0)))
+    if(sin_fi0.lt.0.d0)then
+      B2obs_fi=-B2obs_fi
+    end if
+  end if
+return
+end function B2obs_fi
+
