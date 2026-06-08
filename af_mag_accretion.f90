fa04e33263f84b68c9a02a3f0e38374ca7640de5
alexander_mushtukov <31170808+alexandermushtukov@users.noreply.github.com>
2026-06-08 13:54:22 +0100

    Update af_mag_accretion.f90

--- af_mag_accretion.f90
+++ af_mag_accretion.f90
@@ -118,3 +118,88 @@ contains
 
 end module AstroFun_magnetic_accretion
 
+
+
+!================================================================================================
+! The subroutine calculats the velosity profile in accretion envelope accounting for mass
+! outflow from accretion disc.
+!   mdot - initial mass accretion rate in units of Eddngton mass accretion rates.
+!   mas(i,1) - length, mas(i,2) - beta, mas(i,3) - tau.
+!================================================================================================
+subroutine VelosityProf_2(Lambda,eps_w,mdot,B12,P,m,mas,n,tau_min,t,Rmag8,L39)
+use AstroFun_magnetic_accretion
+implicit none
+real*8::pi=3.141592653589793d0
+real*8,intent(in)::Lambda,eps_w,mdot,B12,P,m
+real*8::mas(n,3)  !== the array contains velosity and tau
+integer,intent(in)::n
+real*8::L39,omega,Rm,H2Rm,omega_k,T_d,beta_0
+real*8::l,dl,r,beta,cos_l,xi,tau,f1,f2,ff,g1,g2,gg,P_tot,P_mag,l_max
+integer::i
+real*8::t, tau_min,R6,Rmag8,mdot_Rm
+
+  R6=1.d0  !==NS radius==!
+
+  omega=2*pi/P
+  call Rmag_SC(mdot,m,R6,B12,Lambda,eps_w,Rmag8,mdot_Rm)
+  !write(*,*)"Rmag8 ",Rmag8
+
+  L39=0.138d0*m*mdot_Rm
+  Rm=Rmag8*1.d8
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  H2Rm=min(0.1d0*L39/m/(Rm/1.d8),0.7d0)
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  omega_k=sqrt(1.33d26*m/Rm**3)
+  T_d=0.25d0*L39**(13./28.)/B12**(3./7.)
+  !beta_0=sqrt(2.d0*T_d/9.d5)
+  !beta_0=Rm/3.d10*sqrt(3.d0/2.d0*(omega_k-omega)**2)
+  beta_0=0.056d0/sqrt(Lambda)*m**(3./7.)*L39**(1./7.)/B12**(2./7.)*abs(1.d0-omega/omega_k)
+  !beta_0=sqrt(m*3.d5/Rm)
+
+  !write(*,*)"# ",beta_0,Rm,omega_k,omega,atan(0.5d0*H2Rm)
+
+  tau_min=1.d10
+  t=0.d0
+  beta=beta_0 !/1.1d0
+  !dl=0.0001d0
+  l=atan(0.5d0*H2Rm)
+  !write(*,*)l,H2Rm
+  !read(*,*)
+  l_max=acos(sqrt(1.d6/Rm))
+  dl=(l_max-l)/(n-1)
+  r=Rm*cos(l)*cos(l)
+  i=1
+  do while(i.le.n)
+    cos_l=cos(l)
+    xi=atan(1.d0/tan(l)/2)
+    tau=0.12d0*L39**(9.d0/7)/B12**(4.d0/7)/beta/Lambda**(1.5d0)/m**(3.d0/14)/cos_l**3
+    !f1=cos(xi)*(1.d0-5.d0*L39/m/tau)  !==with radiation pressure==!
+    f1=cos(xi)                         !==without radiation pressure==!
+    f2=2.6d-3*omega**2*Lambda**3/m**(4.d0/7)*B12**(12.d0/7)/L39**(6.d0/7)*cos_l**7*cos(pi+xi-lambda)
+    ff=2.1d-3*m**(6.d0/7)*sqrt(4.d0-3*cos_l**2)*L39**(2.d0/7)/Lambda/B12**(4.d0/7)/cos_l**3
+    g1=sin(xi)*(1.d0-5.d0/m/tau)
+    g2=2.6d-3*omega**2*Lambda**3/m**(4.d0/7)*B12**(12.d0/7)/L39**(6.d0/7)*cos_l**7*sin(pi+xi-lambda)
+    gg=1.1d10*tau*m*L39**(4.d0/7)/Lambda**2/m**(2.d0/7)/B12**(8.d0/7)/cos_l**4
+    P_tot=gg*(g1+g2)   !==why "-"?
+    P_mag=1.63d13/B12**(10.d0/7)*L39**(12.d0/7)/cos_l**12
+
+    beta=beta+ (dl/beta)*ff*(f1+f2)
+    mas(i,1)=l
+    mas(i,2)=beta
+    mas(i,3)=tau
+    if(tau_min.gt.tau)then
+     tau_min=tau
+    end if
+    if(beta.le.0.d0)then
+      r=13.d0
+    end if
+    l=l+dl
+    r=Rm*cos(l)*cos(l)
+    t=t+dl*Rm*cos(l)*sqrt(4.d0-3.d0*cos(l)*cos(l))/(beta*3.d10)
+    i=i+1
+  end do
+return
+end subroutine VelosityProf_2
+
